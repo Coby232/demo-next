e2be40856fd32f0bba3ec88d61ba6ada6e6a2223
Coby232 <sknukpezah@gmail.com>
2024-11-22 16:16:53 +0000

    path

--- src/app/(pages)/account-opening/personal-account/layout.tsx
+++ src/app/(pages)/account-opening/personal-account/layout.tsx
@@ -12,9 +12,8 @@ const PersonalAccountOpeningLayout = ({
 
   const pathname = usePathname();
 
-const pageTitle = pathname.split('/').pop()?.replace(/-/g, ' ').replace(/\b\w/g, (char) => char.toUpperCase()) || '';
-
-
+  const pageTitle = pathname.split('/').pop()?.replace(/-/g, ' ').replace(/\b\w/g, (char) => char.toUpperCase()) || '';
+  
   return (
     <div className="max-w-xl mx-auto p-6 bg-white shadow-md rounded-lg">
       <h1 className="text-2xl font-semibold text-center mb-2">Open a Personal Bank Account</h1>
