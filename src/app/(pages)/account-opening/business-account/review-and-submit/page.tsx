c8ff9dd0ebca782465d49d20d9339a2691c8b3a8
Coby232 <sknukpezah@gmail.com>
2024-11-27 17:48:02 +0000

    fix

--- src/app/(pages)/account-opening/business-account/review-and-submit/page.tsx
+++ src/app/(pages)/account-opening/business-account/review-and-submit/page.tsx
@@ -1,4 +1,5 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
+"use client"
 import React from 'react';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import { useRouter } from 'next/navigation';
